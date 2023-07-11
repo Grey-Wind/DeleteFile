e1023c54b8f36ee6a620f479e1dbec79fc8992d2
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-11 10:42:11 +0800

    Automatically skips and provides output when there are no files or folders

--- Delete.cs
+++ Delete.cs
@@ -1,21 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
-namespace JetBrainsActivation
+namespace Delete
 {
     public static class DeleteFile
     {
-        // 删除文件或文件夹的函数
         public static void Delete(string name)
         {
             if (Directory.Exists(name))
             {
-                // 如果是文件夹，删除文件夹及其内容
+                // 如果是文件夹，删除文件夹及其内容|If it is a folder, delete the folder and its contents
                 Directory.Delete(name, true);
+                Console.WriteLine("已成功删除文件夹：" + name);
+                Console.WriteLine("The folder was successfully deleted:：" + name);
             }
             else if (File.Exists(name))
             {
-                // 如果是文件，删除文件
+                // 如果是文件，删除文件|If it is a file, delete the file
                 File.Delete(name);
+                Console.WriteLine("已成功删除文件：" + name);
+                Console.WriteLine("The file was successfully deleted:" + name);
+            }
+            else
+            {
+                Console.WriteLine("文件或文件夹不存在：" + name);
+                Console.WriteLine("File or folder does not exist:" + name);
             }
         }
     }
