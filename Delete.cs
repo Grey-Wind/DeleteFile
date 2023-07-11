f4ba75d522b4eeb5e92ea662e3a091d36deade40
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-11 10:25:59 +0800

    Initial commit

---
+++ Delete.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace JetBrainsActivation
+{
+    public static class DeleteFile
+    {
+        // 删除文件或文件夹的函数
+        public static void Delete(string name)
+        {
+            if (Directory.Exists(name))
+            {
+                // 如果是文件夹，删除文件夹及其内容
+                Directory.Delete(name, true);
+            }
+            else if (File.Exists(name))
+            {
+                // 如果是文件，删除文件
+                File.Delete(name);
+            }
+        }
+    }
+}
+
