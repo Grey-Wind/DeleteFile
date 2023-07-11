0c047d25975f49527098f77dec9e3e4d1e88d46e
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-11 10:49:09 +0800

    signature

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DeleteFile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sunrise Studio")]
 [assembly: AssemblyProduct("DeleteFile")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
